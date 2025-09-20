6c4a74f1413d1f5ec36a300ee44ec7241a0b88c4
Supeshan Andruhilmin <supeshan19.20@gmail.com>
2025-09-21 00:10:51 +0200

    edit & deete 1.1

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
         <input type="checkbox" class="checkbox">
         <span>${taskText}</span>
         <div class="task-buttons">
-            <button class="edit-btn"><<i class="fa-solid fa-pen"></i></button>
+            <button class="edit-btn"><i class="fa-solid fa-pen"></i></button>
             <button class="delete-btn"><i class="fa-solid fa-trash"></i></button>
         </div>
         `;
